da05a0022d7e88ba2c18962b57485b383ab9df8e
Manuel Schlestein <manuel.schlestein@gmail.com>
2024-08-20 11:55:40 +0200

    example code

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
 # ixdropdownissue
-example project to demonstrate a bug in IX version 2.4.0
+Example project to demonstrate a bug in IX version 2.4.0
+
+When an ix-dropdown is used inside a ix-tree, it doesnt work anymore. Trigger-mechanism seems to be broken.
+Outside ix-tree it works as expected.
+
+relevant coding to reproduce issue can be found in files
+  - app.component.html
+  - app.component.ts
+
+To start project open terminal and execute the following commands:
+  - npm install
+  - npm run start
+
+then open in browser url http://127.0.0.1:4200/
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { TreeModel } from '@siemens/ix';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,46 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'AngularProject1';
+  title = 'ix-dropdown issue';
+  model: TreeModel<{
+    id: string;
+    name: string;
+  }> = {
+    root: {
+      id: 'root',
+      data: {
+        id: 'root',
+        name: '',
+      },
+      hasChildren: true,
+      children: ['sample'],
+    },
+    sample: {
+      id: 'sample',
+      data: {
+        id: 'sample',
+        name: 'Sample',
+      },
+      hasChildren: true,
+      children: ['sample-child-1', 'sample-child-2'],
+    },
+    'sample-child-1': {
+      id: 'sample-child-1',
+      data: {
+        id: 'sample-child-1',
+        name: 'Sample Child 1',
+      },
+      hasChildren: false,
+      children: [],
+    },
+    'sample-child-2': {
+      id: 'sample-child-2',
+      data: {
+        id: 'sample-child-2',
+        name: 'Sample Child 2',
+      },
+      hasChildren: false,
+      children: [],
+    },
+  };
 }
